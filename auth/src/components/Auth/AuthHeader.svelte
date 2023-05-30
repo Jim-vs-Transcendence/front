41111c96c9dd67fcacab40d0b90f964f106db6c8
Ubuntu <ubuntu@ip-172-31-46-232.ap-northeast-2.compute.internal>
2023-05-30 06:15:44 +0000

    init

---
+++ auth/src/components/Auth/AuthHeader.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { auth, authToken } from "../../service/store";
+    import { goto } from '$app/navigation'
+    import { onMount } from 'svelte';
+
+    const onLogout = () => {
+        authToken.logout();
+        goto('/login');
+    }
+
+    onMount(() => {
+        auth.isLogin();
+    });
+</script>
+
+<header>
+    <div>
+        <h1>헤더 Title</h1>
+        <nav>
+            {#if $auth.email}
+                <button on:click={onLogout}>로그아웃</button>
+            {:else}
+                <a href="/login">로그인</a>
+                <a href="/register">회원가입</a>
+            {/if}
+        </nav>
+    </div>
+</header>
---
+++ auth/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+
+import { goto } from '$app/navigation'
+
+const login = () => {
+        goto('/main');
+    }
+
+</script> 
+
+<button on:click={login}>기록 속으로...</button>
+
+<style>
+
+button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+</style>
