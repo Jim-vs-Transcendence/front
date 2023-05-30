77f28b3e593249112a8372bd36a42f9cc92e633b
Ubuntu <ubuntu@ip-172-31-46-232.ap-northeast-2.compute.internal>
2023-05-30 07:48:22 +0000

    ok

--- auth/src/routes/+page.svelte
+++ auth/src/routes/+page.svelte
@@ -1,34 +1,31 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+  
+  const login = () => {
+    goto('/main');
+  }
+</script>
 
-import { goto } from '$app/navigation'
-
-const login = () => {
-        goto('/main');
-    }
-
-</script> 
-
-<button on:click={login}>기록 속으로...</button>
+<button class="my-button" on:click={login}>기록 속으로...</button>
 
 <style>
-
-button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 16px;
-  cursor: pointer;
-  border-radius: 10px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
+  .my-button {
+    border: none;
+    color: white;
+    padding: 10px 20px; /* smaller padding */
+    text-align: center;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 150px; /* fixed width */
+    height: 50px; /* fixed height */
+  }
 </style>
