a7afabb6cc6042992caca2de61d32771da5c5458
Dongho Hyun <ehdghho@kakao.com>
2023-06-07 14:52:42 +0900

    [DELETE] useless dir

--- auth/src/routes/main/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import AuthHeader from "../../components/Auth/AuthHeader.svelte";
-    import AuthSidebar from "../../components/Auth/AuthSidebar.svelte";
-</script>
-
-<AuthSidebar/>
-<AuthHeader/>
-
-<slot />
--- combine_example/Front/NestJS_Front/client/_app/immutable/chunks/stores.463a5ea7.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./singletons.aa0fa5cb.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
