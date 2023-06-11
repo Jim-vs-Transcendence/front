010f3714a0123477d6dc5fdc73c405285f8d7d04
dhyun <ehdghho@kakao.com>
2023-06-11 05:40:31 +0000

    [REFACT]: main.ts CORS 옵션 삭제
    -현재 사용하지 않는 CORS 옵션 origin, credentials를 삭제했습니다.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from 'src/util/swagger'
+import { setupSwagger } from 'src/util/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.enableCors({});
   app.use(cookieParser());
   setupSwagger(app);
   await app.listen(3000);
